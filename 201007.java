0911393c1f60cab32861911b940d0500f78d900f
Bolozano <65296071+Bolozano@users.noreply.github.com>
2020-10-07 20:36:02 +0200

    Create 201007.java

---
+++ 201007.java
@@ -0,0 +1,89 @@
+/**2. 两数相加
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp1=l1;
+        ListNode temp2=l2;
+        ListNode result=new ListNode();
+        ListNode a=result;
+        int r=0;
+        while((temp1!=null)&&(temp2!=null)){
+            int k=temp1.val+temp2.val;
+            if(k+r>=10){
+
+                a.next=new ListNode(k+r-10);
+                a=a.next;
+                r=1;
+            }
+            else{
+                a.next=new ListNode(k+r);
+                a=a.next;
+                r=0;
+            }
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+        if(temp1==null){
+            while(temp2!=null){
+                int k=temp2.val;
+                if(k+r>=10){
+                    a.next=new ListNode(k+r-10);
+                    a=a.next;
+                    r=1;
+                }
+                else{
+                    a.next=new ListNode(k+r);
+                    a=a.next;
+                    r=0;
+                }
+                temp2=temp2.next;
+            }
+            if(r==1){
+                a.next=new ListNode(1);
+                a=a.next;
+                r=0;
+            }
+        }
+        else{
+            while(temp1!=null){
+                int k=temp1.val;
+                if(k+r>=10){
+                    a.next=new ListNode(k+r-10);
+                    a=a.next;
+                    r=1;
+                }
+                else{
+                    a.next=new ListNode(k+r);
+                    a=a.next;
+                    r=0;
+                }
+                temp1=temp1.next;
+            }
+            if(r==1){
+                a.next=new ListNode(1);
+                a=a.next;
+                r=0;
+            }
+        }
+        return result.next;
+    }
+}
